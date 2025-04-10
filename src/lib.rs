3d1af856236bb54487fd55145d1bf3d419df6868
Ardika Rommy Sanjaya <contact@ardikars.com>
2025-04-10 13:51:25 +0700

    use alias

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,18 @@
 #![no_std]
 extern crate alloc;
 
-pub mod unsigned;
+mod unsigned;
+
+#[allow(non_camel_case_types)]
+pub type u2 = unsigned::u2;
+#[allow(non_camel_case_types)]
+pub type u3 = unsigned::u3;
+#[allow(non_camel_case_types)]
+pub type u4 = unsigned::u4;
+#[allow(non_camel_case_types)]
+pub type u5 = unsigned::u5;
+#[allow(non_camel_case_types)]
+pub type u6 = unsigned::u6;
+#[allow(non_camel_case_types)]
+pub type u7 = unsigned::u7;
+
